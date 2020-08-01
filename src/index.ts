f4d0622f042750ffb50ed8ad022c2d4fd92c82ce
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-08-01 11:36:53 -0700

    Remove penner import

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ export { default as addYears } from './date/add-years';
 
 export { default as clamp } from './numbers/clamp';
 export { default as linearScale } from './numbers/linear-scale';
-export { default as penner } from './numbers/penner';
 
 export { default as formatForDisplay } from './money/format-for-display';
 export { default as smallDisplay } from './money/small-display';
